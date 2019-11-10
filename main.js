7ab044b40bfade5e59c54e9c17e3879263deb0e1
Gustavo Briceno <gustavobrcn@hotmail.com>
2019-11-10 02:48:27 -0500

    Almost completed styling for the navbar. Navbar still needs proper logo and transparent effect.

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log('Java is working');
+// btn1 = document.getElementById('btn-1')
+//
+// btn1.addEventListener('click', () => {
+//   document.getElementById('package1').className = ('package1')
+// })
+
+// variables for elements
+btn0 = document.getElementById('btn-0')
+btn1 = document.getElementById('btn-1')
+btn2 = document.getElementById('btn-2')
+btn3 = document.getElementById('btn-3')
+btn4 = document.getElementById('btn-4')
+pkg1 = document.getElementById('package1')
+pkg2 = document.getElementById('package2')
+pkg3 = document.getElementById('package3')
+pkg4 = document.getElementById('package4')
+
+document.getElementById('package1').className = ('package1')
+document.getElementById('package2').className = ('package2')
+document.getElementById('package3').className = ('package3')
+document.getElementById('package4').className = ('package4')
